05e5bd85e662829e092605fb6cb5c8351b7436f3
xiaonan0882-arch <xiaonan0882@gmail.com>
2026-09-20 14:16:32 +0800

    V14: refresh PWA cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='asset-note-v13';const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const C='asset-note-v14';const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request))));
